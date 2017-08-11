69458c64c1ee0dc8e0d3f9ae5be5e578b09de1b8
Prabu Sankar R <prabu@emdsys.com>
2017-08-11 16:49:33 +0530

    simple bug fixes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 // function to encode file data to base64 encoded string
 module.exports = function () {
-    function base64_encode(file) {
+    this.base64Encode = (file) => {
         // read binary data
         var bitmap = fs.readFileSync(file);
         // convert binary data to base64 encoded string
         return new Buffer(bitmap).toString('base64');
     }
 // function to create file from base64 encoded string
-    function base64_decode(base64str, file) {
+    this.base64Decode =  (base64str, file) => {
         // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
         var bitmap = new Buffer(base64str, 'base64');
         // write buffer to file
